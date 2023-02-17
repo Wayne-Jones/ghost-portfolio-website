541b818263aa1025dbb37710bdf166bd88150db2
Wayne Jones <2455598+Wayne-Jones@users.noreply.github.com>
2023-02-17 01:14:28 -0500

    Adding Montserrat font and Unsplash functionality utilizing next/font and next/image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 
 import Link from 'next/link'
+import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 import { getAllPosts } from '../helper/util'
 import { Post } from '../helper/types'
@@ -16,7 +17,17 @@ export default function Home(props: { posts: Post[] }) {
   const { posts } = props;
   return (
     <>
-      <div className="w-full flex flex-col justify-center items-center">
+      <section className="min-h-screen container flex flex-col lg:flex-row items-center justify-between gap-5 py-12">
+        <div className="flex flex-col items-center lg:items-start justify-center w-full lg:w-1/2 text-center lg:text-start">
+          <h1 className="mb-4">Hi, I'm Wayne Jones</h1>
+          <p className="mb-4">I am a web developer and content creator based in New York City</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore perspiciatis incidunt facilis laborum iste consequuntur inventore porro veritatis ipsa placeat? Quos sed aliquam eaque laudantium sapiente explicabo, modi asperiores aliquid! Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore perspiciatis incidunt facilis laborum iste consequuntur inventore porro veritatis ipsa placeat? Quos sed aliquam eaque laudantium sapiente explicabo, modi asperiores aliquid!</p>
+        </div>
+        <div className="flex w-full lg:w-1/2 justify-center lg:justify-end">
+          <Image src="http://source.unsplash.com/random/500x700" alt="Random Picture" className="object-cover max-h-[300px] lg:max-h-[600px]" width="500" height="700"/>
+        </div>
+      </section>
+      <section className="min-h-screen container flex flex-col">
         <h1 className="text-3xl font-bold underline">Blog</h1>
         <ul className="text-center">
           {posts.map((post) =>{
@@ -27,7 +38,7 @@ export default function Home(props: { posts: Post[] }) {
             </li>
           })}
         </ul>
-      </div>
+      </section>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
+// tailwind.config.js
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-montserrat)', ...fontFamily.sans],
+      }
+    },
   },
   plugins: [],
 }
