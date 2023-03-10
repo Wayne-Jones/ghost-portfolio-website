8e7823c19b0979984c90b126a01c84667c7ab6cc
Wayne Jones <2455598+Wayne-Jones@users.noreply.github.com>
2023-03-10 00:30:31 -0500

    Fixed navigation to implement light/dark theme

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Dialog } from "@headlessui/react";
 import { BiMenuAltRight } from "react-icons/bi";
 import { HiXMark } from "react-icons/hi2";
+import Link from "next/link";
 
 type Props = {};
 
@@ -17,25 +18,20 @@ const Header = (props: Props) => {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
     return (
-        <header className="bg-white dark:bg-black">
+        <header className="container absolute left-0 right-0">
             <nav
-                className="flex max-w-full items-center justify-between p-6 lg:px-8"
+                className="flex max-w-full items-center justify-between py-6"
                 aria-label="Navigation">
-                <a href="#" className="-m-1.5 p-1.5">
-                    <span className="sr-only">Your Company</span>
-                    <img
-                        className="h-8 w-auto"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                        alt=""
-                    />
-                </a>
+                <Link href="/" className="uppercase text-2xl font-bold">
+                    Wayne Jones
+                </Link>
                 <div className="flex lg:hidden">
                     <button
                         type="button"
-                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 dark:text-gray-100"
+                        className="-m-1.5 inline-flex items-center justify-center rounded-md p-1.5 "
                         onClick={() => setMobileMenuOpen(true)}>
                         <span className="sr-only">Open main menu</span>
-                        <BiMenuAltRight className="h-6 w-6" aria-hidden="true" />
+                        <BiMenuAltRight className="h-8 w-8" aria-hidden="true" />
                     </button>
                 </div>
                 <div className="hidden lg:flex lg:gap-x-12">
@@ -43,7 +39,7 @@ const Header = (props: Props) => {
                         <a
                             key={item.name}
                             href={item.href}
-                            className="text-base text-gray-900 dark:text-gray-100">
+                            className="text-base">
                             {item.name}
                         </a>
                     ))}
@@ -55,31 +51,26 @@ const Header = (props: Props) => {
                 open={mobileMenuOpen}
                 onClose={setMobileMenuOpen}>
                 <div className="fixed inset-0 z-10" />
-                <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white dark:bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto px-8 py-6 sm:max-w-sm bg-white dark:bg-dark-gray">
                     <div className="flex items-center justify-between">
-                        <a href="#" className="-m-1.5 p-1.5">
-                            <span className="sr-only">Your Company</span>
-                            <img
-                                className="h-8 w-auto"
-                                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                                alt=""
-                            />
-                        </a>  
+                        <Link href="/" className="uppercase text-2xl font-bold">
+                            Wayne Jones
+                        </Link>
                         <button
                             type="button"
-                            className="-m-2.5 rounded-md p-2.5 text-gray-700 dark:text-gray-100"
+                            className="-m-1.5 rounded-md p-1.5"
                             onClick={() => setMobileMenuOpen(false)}>
                             <span className="sr-only">Close menu</span>
-                            <HiXMark className="h-6 w-6" aria-hidden="true" />
+                            <HiXMark className="h-8 w-8" aria-hidden="true" />
                         </button>
                     </div>
-                    <div className="mt-6 flow-root">
-                        <div className="space-y-2 py-6">
+                    <div className="mt-6">
+                        <div className="space-y-2 py-6 flex flex-col">
                             {navigation.map((item) => (
                                 <a
                                     key={item.name}
                                     href={item.href}
-                                    className="-mx-3 block rounded-lg py-2 px-3 text-base text-gray-900 dark:text-gray-100 hover:bg-gray-50">
+                                    className="-mx-3 py-2 px-3 text-base">
                                     {item.name}
                                 </a>
                             ))}
