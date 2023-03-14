2545b69f599c506065c967698a8c35442bb47c50
Wayne Jones <2455598+Wayne-Jones@users.noreply.github.com>
2023-03-14 16:34:45 -0400

    Adding hero margins

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,7 +5,7 @@ import Button from "./button";
 const Hero = () => {
     return (
         <section className="min-h-screen dark:bg-gradient-to-r dark:from-light-gray dark:to-dark-gray flex items-center">
-            <div className="container flex flex-col md:flex-row items-center justify-between gap-5 py-24">
+            <div className="container flex flex-col md:flex-row items-center justify-between gap-5 py-24 my-12">
                 <div className="w-full md:w-1/2">
                     <h2 className="mb-6 text-4xl md:text-5xl !leading-tight font-bold tracking-tight lg:text-6xl xl:text-7xl">Experienced Web Developer & Photographer</h2>
                     <Button text="Get In Touch" icon={<HiOutlineArrowRight className="w-6 h-6" />}/>
