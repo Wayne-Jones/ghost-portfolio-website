7ac666e7b1d964c4104c3adf6d0e0330036f30fb
Wayne Jones <2455598+Wayne-Jones@users.noreply.github.com>
2023-03-14 16:35:09 -0400

    Allowing additional classes to be passed down via props

--- src/components/aside.tsx
+++ src/components/aside.tsx
@@ -1,12 +1,14 @@
 type Props = {
+    className?: string,
     text: string,
 }
 
 const Aside = (props: Props) => {
+  const classes = `border-l-2 border-dark-purple dark:border-light-purple px-3 py-2 ${props.className ? props.className: ""}`;
   return (
-    <div className="border-l-2 border-dark-purple dark:border-light-purple px-3 py-2">
+    <p className={classes}>
       {props.text}
-    </div>
+    </p>
   )
 }
 
