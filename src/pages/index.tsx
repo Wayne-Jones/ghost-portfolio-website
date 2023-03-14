e1d9e2e28d4b1423972c0800ce585f78be760e32
Wayne Jones <2455598+Wayne-Jones@users.noreply.github.com>
2023-03-14 16:35:24 -0400

    Creation of about section component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
-
-import Link from 'next/link'
-import styles from '@/styles/Home.module.css'
 import { getAllPosts } from '../helper/util'
 import { Post } from '../helper/types'
 import Header from '@/components/header'
 import Hero from '@/components/hero'
+import About from '@/components/about'
+import Link from 'next/link'
 
 export const getStaticProps = async () => {
   const posts = await getAllPosts()
@@ -18,23 +17,14 @@ export default function Home(props: { posts: Post[] }) {
   const { posts } = props;
   return (
     <>
-      <Header></Header>
-      <Hero></Hero>
-      {/* <section className="min-h-screen container flex flex-col lg:flex-row items-center justify-between gap-5 py-12">
-        <div className="flex flex-col items-center lg:items-start justify-center w-full lg:w-1/2 text-center lg:text-start">
-          <h1 className="mb-4">Hi, I&#39;m Wayne Jones</h1>
-          <p className="mb-4">I am a web developer and content creator based in New York City</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore perspiciatis incidunt facilis laborum iste consequuntur inventore porro veritatis ipsa placeat? Quos sed aliquam eaque laudantium sapiente explicabo, modi asperiores aliquid! Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore perspiciatis incidunt facilis laborum iste consequuntur inventore porro veritatis ipsa placeat? Quos sed aliquam eaque laudantium sapiente explicabo, modi asperiores aliquid!</p>
-        </div>
-        <div className="flex w-full lg:w-1/2 justify-center lg:justify-end">
-          <Image src="http://source.unsplash.com/random/500x700" alt="Random Picture" className="object-cover max-h-[300px] lg:max-h-[600px]" width="500" height="700"/>
-        </div>
-      </section> */}
+      <Header/>
+      <Hero/>
+      <About/>
       <section className="min-h-screen container flex flex-col">
         <h1 className="text-3xl font-bold underline">Blog</h1>
         <ul className="text-center">
           {posts.map((post) =>{
-            return <li className={styles.postitem} key={post.slug}>
+            return <li key={post.slug}>
               <Link href="/post/[slug]" as={`/post/${post.slug}`}>
                 {post.title}
               </Link>
