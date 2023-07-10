644cccb608ec585350e3d8d26190169fbfd6d243
Wayne Jones <wej916@gmail.com>
2023-07-09 20:09:18 -0400

    Creating portfolio component and adding react-splide

---
+++ src/components/portfolio.tsx
@@ -0,0 +1,14 @@
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+const Portfolio = () => {
+    return (
+        <section className="dark:bg-dark-gray relative my-4 py-4">
+            <div className="container">
+                <div className="w-full">
+                    <h2 className="text-dark-purple dark:text-light-purple text-3xl font-semibold uppercase">Portfolio</h2>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Portfolio
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Post } from '../helper/types'
 import Header from '@/components/header'
 import Hero from '@/components/hero'
 import About from '@/components/about'
+import Portfolio from '@/components/portfolio'
 import Link from 'next/link'
 
 export const getStaticProps = async () => {
@@ -20,6 +21,7 @@ export default function Home(props: { posts: Post[] }) {
       <Header/>
       <Hero/>
       <About/>
+      <Portfolio/>
       <section className="min-h-screen container flex flex-col">
         <h1 className="text-3xl font-bold underline">Blog</h1>
         <ul className="text-center">
