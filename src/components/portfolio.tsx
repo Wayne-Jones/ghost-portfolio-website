6d836b256d9bce0bfcd60ffe69dae88e0e110dc8
Wayne Jones <2455598+Wayne-Jones@users.noreply.github.com>
2023-09-03 19:12:36 -0400

    Adding Splide track to portfolio component

--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -1,11 +1,50 @@
-import { Splide, SplideSlide } from '@splidejs/react-splide';
-const Portfolio = () => {
+import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css/core';
+import { Post } from '@/helper/types';
+import Image from 'next/image';
+
+const Portfolio = (props: {posts: Post[]}) => {
+    const { posts } = props;
     return (
         <section className="dark:bg-dark-gray relative my-4 py-4">
             <div className="container">
                 <div className="w-full">
-                    <h2 className="text-dark-purple dark:text-light-purple text-3xl font-semibold uppercase">Portfolio</h2>
+                    <h2 className="text-dark-purple dark:text-light-purple text-3xl font-semibold uppercase" id="portfolio">Portfolio</h2>
                 </div>
+                <Splide 
+                    hasTrack={ false }
+                    aria-labelledby="portfolio"
+                    options={{
+                        heightRatio: 0.5,
+                        width: 800,
+                        gap: "1em",
+                        padding: "1rem",
+                        drag: false,
+                        type: "loop"
+                    }}
+                >
+                    <SplideTrack>
+                        {posts.map((post)=>{
+                            return (
+                                <SplideSlide key={post.slug}>
+                                    <Image
+                                    src={post.feature_image}
+                                    alt={post.feature_image_alt}
+                                    fill
+                                    objectFit='cover'
+                                    layout='fill'/>
+                                </SplideSlide>
+                            )
+                        })}
+                    </SplideTrack>
+                    <div className="splide__progress">
+                        <div className="splide__progress__bar" />
+                    </div>
+                    <div className="splide__arrows">
+                        <button className="splide__arrow splide__arrow--prev">Prev</button>
+                        <button className="splide__arrow splide__arrow--next">Next</button>
+                    </div>
+                </Splide>
             </div>
         </section>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,15 @@
-import { getAllPosts } from '../helper/util'
+import { getAllPostsByTag } from '../helper/util'
 import { Post } from '../helper/types'
 import Header from '@/components/header'
 import Hero from '@/components/hero'
 import About from '@/components/about'
 import Portfolio from '@/components/portfolio'
-import Link from 'next/link'
 
 export const getStaticProps = async () => {
-  const posts = await getAllPosts()
+  const posts = await getAllPostsByTag('portfolio');
   return {
-    props: { posts },
-    revalidate: 10
+      props: { posts },
+      revalidate: 10
   }
 }
 
@@ -21,19 +20,7 @@ export default function Home(props: { posts: Post[] }) {
       <Header/>
       <Hero/>
       <About/>
-      <Portfolio/>
-      <section className="min-h-screen container flex flex-col">
-        <h1 className="text-3xl font-bold underline">Blog</h1>
-        <ul className="text-center">
-          {posts.map((post) =>{
-            return <li key={post.slug}>
-              <Link href="/post/[slug]" as={`/post/${post.slug}`}>
-                {post.title}
-              </Link>
-            </li>
-          })}
-        </ul>
-      </section>
+      <Portfolio posts={posts}/>
     </>
   )
 }
