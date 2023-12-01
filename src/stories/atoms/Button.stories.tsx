e9123775fe7a14185374cd4d30ce65a466ed57ab
Wayne Jones <2455598+Wayne-Jones@users.noreply.github.com>
2023-12-01 18:33:36 -0500

    Adding Dark Mode variant functionality

--- src/stories/atoms/Button.stories.tsx
+++ src/stories/atoms/Button.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import Button from "@/components/button";
+import Button from "@/components/Button";
 import { HiOutlineArrowRight } from "react-icons/hi2";
 
 const meta = {
@@ -16,7 +16,13 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-const Icon: JSX.Element = <HiOutlineArrowRight className="w-6 h-6"/>;
+const Icon: JSX.Element = <HiOutlineArrowRight className="w-6 h-6" />;
+const toggleTheme = () => {
+  const currentTheme = document.documentElement.className;
+  document.documentElement.classList.toggle('dark');
+  document.documentElement.classList.toggle('light');
+  localStorage.theme = currentTheme.includes('dark') ? 'light' : 'dark';
+}
 
 export const Default: Story = {
   args: {
@@ -43,6 +49,14 @@ export const IconOnly: Story = {
   args: {
     text: "",
     icon: Icon
+  },
+};
+
+export const DarkModeToggle: Story = {
+  args: {
+    text: "",
+    icon: Icon,
+    onClick: toggleTheme
   },
 };
 
