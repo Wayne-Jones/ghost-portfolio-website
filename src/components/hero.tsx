65c6029b7a0ec306def1c6f3c8bed93d2e1ba9ff
Wayne Jones <2455598+Wayne-Jones@users.noreply.github.com>
2023-12-05 20:42:22 -0500

    Fixing Button rename import

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,23 +1,23 @@
 import { HiOutlineArrowRight } from "react-icons/hi2";
 import Image from "next/image";
 import Aside from "./aside";
-import Button from "./button";
+import Button from "./Button";
 
 const Hero = () => {
     return (
         <section className="relative min-h-screen dark:bg-gradient-to-r dark:from-light-gray dark:to-dark-gray flex items-center">
-            <div className="absolute right-0 h-full hidden lg:block w-3/5 overflow-hidden opacity-50" style={{clipPath: "polygon(150px 0%, 100% 0%, 100% 100%, 0% 100%)"}}>
+            <div className="absolute right-0 h-full hidden lg:block w-3/5 overflow-hidden opacity-50" style={{ clipPath: "polygon(150px 0%, 100% 0%, 100% 100%, 0% 100%)" }}>
                 <Image
                     src="https://picsum.photos/1000"
                     alt="Random Image"
                     fill
-                    style={{objectFit: "cover"}}/>
-                    
+                    style={{ objectFit: "cover" }} />
+
             </div>
             <div className="container flex flex-col lg:flex-row items-center justify-between gap-5 my-32 z-[1]">
                 <div className="w-full lg:w-1/2">
                     <h2 className="mb-6 text-4xl md:text-5xl !leading-tight font-bold tracking-tight xl:text-6xl">Experienced Web Developer & Photographer</h2>
-                    <Button text="Get In Touch" icon={<HiOutlineArrowRight className="w-6 h-6" />}/>
+                    <Button text="Get In Touch" icon={<HiOutlineArrowRight className="w-6 h-6" />} />
                     <p className="mt-24 text-lg leading-8">
                         Based in New York City
                     </p>
