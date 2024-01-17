2948da1e8bd7ae59098f70ffcd311662ba5a8c1e
Wayne Jones <2455598+Wayne-Jones@users.noreply.github.com>
2024-01-17 00:09:12 -0500

    Consolidation and fixing hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -19,63 +19,73 @@ const Hero = (props: Props) => {
     const bgImage = props.bgImage;
     const cta = props.cta;
     return (
-        <section className="relative min-h-screen dark:bg-light-gray py-10">
-            {align === 'center' &&
-                <div className="container flex flex-col items-center justify-center gap-5">
-                    <div className="flex flex-col items-center justify-center flex-nowrap">
-                        <h1 className="mb-6 text-4xl md:text-5xl !leading-tight font-bold tracking-tight xl:text-6xl">Wayne Jones</h1>
-                        <h2 className="mb-6 text-xl md:text-2xl !leading-tight font-bold tracking-tight xl:text-3xl text-center">Experienced Web Developer & Photographer</h2>
-                        <p className="text-lg leading-8">
-                            Based in New York City
-                        </p>
-                        {cta && <Button text="Get In Touch" onClick={()=> {window.location.href = cta;}}icon={<HiOutlineArrowRight className="w-6 h-6" />} /> }
+        <section className="min-h-screen dark:bg-light-gray py-10 flex justify-center items-center" style={{ backgroundImage: `url(${bgImage})`, backgroundRepeat: "no-repeat", backgroundPosition: "center", backgroundSize: "cover"}}>
+            <div className="container">
+                {align === 'center' &&
+                    <div className="flex flex-col justify-center items-center gap-12">
+                        <div className="flex flex-col items-center justify-center">
+                            <h1 className="mb-6 text-4xl md:text-5xl !leading-tight font-bold tracking-tight xl:text-6xl">Wayne Jones</h1>
+                            <h2 className="mb-6 text-xl md:text-2xl !leading-tight font-bold tracking-tight xl:text-3xl">Experienced Web Developer & Photographer</h2>
+                            <p className="text-lg leading-8">
+                                Based in New York City
+                            </p>
+                            {cta && <Button text="Get In Touch" onClick={()=> {window.location.href = cta;}} icon={<HiOutlineArrowRight className="w-6 h-6" />} /> }
+                        </div>
+                        {inlineImage && 
+                            <div className="max-w-lg">
+                                <Image
+                                    src={inlineImage}
+                                    alt={inlineAlt ?? ""}
+                                    width={500}
+                                    height={200}
+                                    style={{ objectFit: "cover", width: "100%", height: "auto" }} />
+                            </div>
+                        }
                     </div>
-                    {inlineImage && 
+                }
+                {align === 'left' &&
+                    <div className="flex justify-center gap-32">
+                        <div className="flex flex-col items-center justify-center">
+                            <h1 className="mb-6 text-4xl md:text-5xl !leading-tight font-bold tracking-tight xl:text-6xl">Wayne Jones</h1>
+                            <h2 className="mb-6 text-xl md:text-2xl !leading-tight font-bold tracking-tight xl:text-3xl">Experienced Web Developer & Photographer</h2>
+                            <p className="mb-6 text-lg leading-8">
+                                Based in New York City
+                            </p>
+                            {cta && <Button text="Get In Touch" onClick={()=> {window.location.href = cta;}} icon={<HiOutlineArrowRight className="w-6 h-6" />} />}
+                        </div>
+                        {inlineImage && 
                         <div className="max-w-lg">
                             <Image
-                                src={inlineImage}
-                                alt={inlineAlt ?? ""}
-                                width={500}
-                                height={200}
-                                style={{ objectFit: "cover", width: "100%", height: "auto" }} />
+                            src={inlineImage}
+                            alt={inlineAlt ?? ""}
+                            width={500}
+                            height={200}
+                            style={{ objectFit: "cover" }} />
+                        </div>}
+                    </div>
+                }
+                {align === 'right' &&
+                    <div className="flex justify-center gap-32">
+                        {inlineImage && 
+                        <div className="max-w-lg">
+                            <Image
+                            src={inlineImage}
+                            alt={inlineAlt ?? ""}
+                            width={500}
+                            height={200}
+                            style={{ objectFit: "cover" }} />
+                        </div>}
+                        <div className="flex flex-col items-center justify-center">
+                            <h1 className="mb-6 text-4xl md:text-5xl !leading-tight font-bold tracking-tight xl:text-6xl">Wayne Jones</h1>
+                            <h2 className="mb-6 text-xl md:text-2xl !leading-tight font-bold tracking-tight xl:text-3xl">Experienced Web Developer & Photographer</h2>
+                            <p className="text-lg leading-8">
+                                Based in New York City
+                            </p>
+                            {cta && <Button text="Get In Touch" onClick={()=> {window.location.href = cta;}} icon={<HiOutlineArrowRight className="w-6 h-6" />} />}
                         </div>
-                    }
-                </div>
-            }
-            {align === 'left' &&
-                <div className="container flex">
-                    <div className="flex flex-col items-center justify-center flex-nowrap">
-                        <h1 className="mb-6 text-4xl md:text-5xl !leading-tight font-bold tracking-tight xl:text-6xl">Wayne Jones</h1>
-                        <h2 className="mb-6 text-xl md:text-2xl !leading-tight font-bold tracking-tight xl:text-3xl text-center">Experienced Web Developer & Photographer</h2>
-                        <p className="mt-24 text-lg leading-8">
-                            Based in New York City
-                        </p>
-                        {cta && <Button text="Get In Touch" icon={<HiOutlineArrowRight className="w-6 h-6" />} />}
                     </div>
-                    {inlineImage && <Image
-                        src={inlineImage}
-                        alt={inlineAlt ?? ""}
-                        fill
-                        style={{ objectFit: "cover" }} />}
-                </div>
-            }
-            {align === 'right' &&
-                <div className="container flex">
-                    {inlineImage && <Image
-                        src={inlineImage}
-                        alt={inlineAlt ?? ""}
-                        fill
-                        style={{ objectFit: "cover" }} />}
-                    <div className="flex flex-col items-center justify-center flex-nowrap">
-                        <h1 className="mb-6 text-4xl md:text-5xl !leading-tight font-bold tracking-tight xl:text-6xl">Wayne Jones</h1>
-                        <h2 className="mb-6 text-xl md:text-2xl !leading-tight font-bold tracking-tight xl:text-3xl text-center">Experienced Web Developer & Photographer</h2>
-                        <p className="mt-24 text-lg leading-8">
-                            Based in New York City
-                        </p>
-                        {cta && <Button text="Get In Touch" icon={<HiOutlineArrowRight className="w-6 h-6" />} />}
-                    </div>
-                </div>
-            }
+                }
+            </div>
         </section>
     )
 }
