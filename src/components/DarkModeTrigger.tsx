c08b84f509727c507f4eaf3144680d0ec6fa91ad
Wayne Jones <2455598+Wayne-Jones@users.noreply.github.com>
2024-05-13 00:46:01 -0400

    Creating a darkmode trigger button component

---
+++ src/components/DarkModeTrigger.tsx
@@ -0,0 +1,36 @@
+import Button from '@/components/Button';
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
+import { MdOutlineDarkMode, MdOutlineLightMode } from 'react-icons/md';
+import { VscBlank } from 'react-icons/vsc';
+
+const DarkModeTrigger = () => {
+  const [mounted, setMounted] = useState(false);
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === 'system' ? systemTheme : theme;
+
+  const toggleTheme = (event: React.MouseEvent, currentTheme: string | undefined) => {
+    currentTheme === 'dark' ? setTheme('light') : setTheme('dark');
+  };
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) {
+    return <Button text='' icon={<VscBlank className='w-6 h-6' />} />;
+  }
+  return (
+    <Button
+      text=''
+      icon={
+        currentTheme !== 'dark' ? (
+          <MdOutlineDarkMode className='w-6 h-6' />
+        ) : (
+          <MdOutlineLightMode className='w-6 h-6' />
+        )
+      }
+      onClick={(e) => toggleTheme(e, currentTheme)}
+    />
+  );
+};
+
+export default DarkModeTrigger;
