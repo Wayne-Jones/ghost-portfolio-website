c7ea9b89f2da3a6d8189baa75fab7696d5843120
Wayne Jones <2455598+Wayne-Jones@users.noreply.github.com>
2024-05-13 00:46:34 -0400

    Modifying styling and functionality for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
-import { Dialog } from '@headlessui/react';
+'use client';
+import { useState } from 'react';
+import { Dialog, DialogPanel } from '@headlessui/react';
 import { BiMenuAltRight } from 'react-icons/bi';
 import { HiXMark } from 'react-icons/hi2';
 import Link from 'next/link';
-import Button from './Button';
-import { MdOutlineLightMode, MdOutlineDarkMode } from 'react-icons/md';
+import DarkModeTrigger from './DarkModeTrigger';
 
 const navigation = [
   { name: 'Home', href: '/' },
@@ -16,74 +16,52 @@ const navigation = [
 
 const Header = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-  const [darkMode, setDarkMode] = useState(false);
-  const toggleTheme = () => {
-    const currentTheme = document.documentElement.className;
-    if (currentTheme.includes('dark')) {
-      document.documentElement.classList.remove('dark');
-      document.documentElement.classList.add('light');
-      localStorage.theme = 'light';
-    } else {
-      document.documentElement.classList.remove('light');
-      document.documentElement.classList.add('dark');
-      localStorage.theme = 'dark';
-    }
-    setDarkMode(!darkMode);
-  };
-
-  useEffect(() => {
-    const currentTheme = document.documentElement.className;
-    setDarkMode(currentTheme.includes('dark') ? true : false);
-  }, []);
 
   return (
-    <header className='container'>
+    <header className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
       <nav className='flex w-full items-center justify-between py-6' aria-label='Navigation'>
         <Link href='/' className='uppercase text-2xl font-bold'>
           Wayne Jones
         </Link>
-        <div className='hidden lg:flex gap-x-8 xl:gap-x-12'>
+        <div className='hidden md:flex items-center gap-x-6 xl:gap-x-8'>
           {navigation.map((item) => (
             <a key={item.name} href={item.href} className='text-base'>
               {item.name}
             </a>
           ))}
+          <DarkModeTrigger />
         </div>
-        {!darkMode && (
-          <Button text='' icon={<MdOutlineDarkMode className='w-6 h-6' />} onClick={toggleTheme} />
-        )}
-        {darkMode && (
-          <Button text='' icon={<MdOutlineLightMode className='w-6 h-6' />} onClick={toggleTheme} />
-        )}
-        <div className='flex lg:hidden'>
+        <div className='flex md:hidden p-3'>
           <button
             type='button'
             className='-m-1.5 inline-flex items-center justify-center rounded-md p-1.5 '
-            onClick={() => setMobileMenuOpen(true)}
+            onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
           >
             <span className='sr-only'>Open main menu</span>
             <BiMenuAltRight className='h-8 w-8' aria-hidden='true' />
           </button>
         </div>
       </nav>
-      <Dialog as='div' className='lg:hidden' open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+      <Dialog as='div' className='md:hidden' open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className='fixed inset-0 z-10' />
-        <Dialog.Panel className='fixed inset-y-0 right-0 z-10 w-full overflow-y-auto px-8 py-6 sm:max-w-sm bg-white dark:bg-dark-gray'>
+        <DialogPanel className='fixed inset-y-0 right-0 z-10 w-full overflow-y-auto px-4 sm:px-6 py-6 bg-white dark:bg-dark-gray'>
           <div className='flex items-center justify-between'>
             <Link href='/' className='uppercase text-2xl font-bold'>
               Wayne Jones
             </Link>
-            <button
-              type='button'
-              className='-m-1.5 rounded-md p-1.5'
-              onClick={() => setMobileMenuOpen(false)}
-            >
-              <span className='sr-only'>Close menu</span>
-              <HiXMark className='h-8 w-8' aria-hidden='true' />
-            </button>
+            <div className='flex p-3'>
+              <button
+                data-autofocus
+                className='-m-1.5 inline-flex items-center justify-center rounded-md p-1.5'
+                onClick={() => setMobileMenuOpen(false)}
+              >
+                <span className='sr-only'>Close mobile menu</span>
+                <HiXMark className='h-8 w-8' aria-hidden='true' />
+              </button>
+            </div>
           </div>
-          <div className='mt-6'>
-            <div className='space-y-2 py-6 flex flex-col'>
+          <div className=''>
+            <div className='space-y-2 flex flex-col'>
               {navigation.map((item) => (
                 <a key={item.name} href={item.href} className='-mx-3 py-2 px-3 text-base'>
                   {item.name}
@@ -91,7 +69,7 @@ const Header = () => {
               ))}
             </div>
           </div>
-        </Dialog.Panel>
+        </DialogPanel>
       </Dialog>
     </header>
   );
