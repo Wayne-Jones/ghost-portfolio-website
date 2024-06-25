622ba0ec18019a1ece2cfa5c107e94a1fc2c24ac
Wayne Jones <2455598+Wayne-Jones@users.noreply.github.com>
2024-06-25 18:22:37 -0400

    Editing tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,40 +1,47 @@
-import type { Config } from "tailwindcss"
+import type { Config } from 'tailwindcss';
+const { fontFamily } = require('tailwindcss/defaultTheme');
 
 const config = {
-  darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-  ],
-  prefix: "",
+  darkMode: ['class'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  prefix: '',
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: '2rem',
       screens: {
-        "2xl": "1400px",
-      },
+        '2xl': '1400px'
+      }
     },
     extend: {
+      fontFamily: {
+        sans: ['var(--font-montserrat)', ...fontFamily.sans]
+      },
       keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' }
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' }
+        }
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out'
       },
-    },
+      colors: {
+        'light-purple': '#A994FF',
+        'dark-purple': '#4C20FE',
+        'light-gray': '#2F2F30',
+        'dark-gray': '#161517',
+        white: '#FFFFFF',
+        black: '#000000'
+      }
+    }
   },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config
+  plugins: [require('tailwindcss-animate')]
+} satisfies Config;
 
-export default config
+export default config;
