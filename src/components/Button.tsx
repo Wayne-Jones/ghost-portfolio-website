31d2084bda66fec13002021f187454cc530c9343
Wayne Jones <2455598+Wayne-Jones@users.noreply.github.com>
2024-06-27 16:07:36 -0400

    Editing existing button component to include shadcn button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,33 +1,39 @@
 import { MouseEventHandler } from 'react';
+import { Button as UIButton } from "@/components/ui/button";
 
 type Props = {
   text: string;
   icon?: JSX.Element;
   invert?: boolean;
-  onClick?: MouseEventHandler;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  variant?: "default" | "outline" | "ghost"
 };
 
 const Button = (props: Props) => {
   const Icon = props.icon;
+  const variant = props.variant ?? "default";
   const Invert = props.invert ?? false;
   const clickFunction = props.onClick;
+  const label = props.text;
   return (
     <>
       {!Invert && (
-        <button
+        <UIButton
           className='bg-dark-purple dark:bg-light-purple text-white dark:text-dark-gray p-4 uppercase text-base rounded-md font-bold flex items-center gap-2'
           onClick={clickFunction}
+          variant={variant}
         >
-          {props.text} {Icon}
-        </button>
+          {label} {Icon}
+        </UIButton>
       )}
       {Invert && (
-        <button
+        <UIButton
           className='bg-dark-purple dark:bg-light-purple text-white dark:text-dark-gray p-4 uppercase text-base rounded-md font-bold flex items-center gap-2'
           onClick={clickFunction}
+          variant={variant}
         >
-          {Icon} {props.text}
-        </button>
+          {Icon} {label}
+        </UIButton>
       )}
     </>
   );
