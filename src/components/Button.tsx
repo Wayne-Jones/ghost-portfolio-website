9d5680ff2038d81f77d446fa9f4dc3614ea002ec
Wayne Jones <2455598+Wayne-Jones@users.noreply.github.com>
2024-06-28 15:54:13 -0400

    Fixing button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,12 @@
-import { Button as UIButton } from "@/components/ui/button";
-import React from "react";
+import { Button as UIButton } from '@/components/ui/button';
+import React from 'react';
 
-interface ButtonProps
-extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   text: string;
   icon?: JSX.Element;
   invert?: boolean;
   hideText?: boolean;
-  variant?: "default" | "outline";
+  variant?: 'default' | 'outline';
 }
 
 const Button = (props: ButtonProps) => {
@@ -18,17 +17,15 @@ const Button = (props: ButtonProps) => {
   return (
     <>
       {!Invert && (
-        <UIButton
-          {...props}
-        >
-          {hideText ? (<span className="sr-only">{label}</span>) : label} {Icon}
+        <UIButton {...props}>
+          {hideText === true ? <span className='sr-only'>{label}</span> : label}
+          {Icon}
         </UIButton>
       )}
       {Invert && (
-        <UIButton
-          {...props}
-        >
-          {Icon} {hideText ? (<span className="sr-only">{label}</span>) : label}
+        <UIButton {...props}>
+          {Icon}
+          {hideText === true ? <span className='sr-only'>{label}</span> : label}
         </UIButton>
       )}
     </>
