b8ff75e7a347765ddd6301b2b22e2086ce44e4d6
Wayne Jones <2455598+Wayne-Jones@users.noreply.github.com>
2024-06-28 17:35:18 -0400

    Refactoring nav header to include shadcn ui sheet

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,9 @@
 'use client';
 import { useState } from 'react';
-import { Dialog, DialogPanel } from '@headlessui/react';
 import { BiMenuAltRight } from 'react-icons/bi';
-import { HiXMark } from 'react-icons/hi2';
 import Link from 'next/link';
-import DarkModeTrigger from './DarkModeTrigger';
+import DarkModeTrigger from '@/components/DarkModeTrigger';
+import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
 
 const navigation = [
   { name: 'Home', href: '/' },
@@ -18,59 +17,60 @@ const Header = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
-      <nav className='flex w-full items-center justify-between py-6' aria-label='Navigation'>
+    <header className='flex mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-6'>
+      <nav className='flex w-full items-center justify-between ' aria-label='Navigation'>
         <Link href='/' className='uppercase text-2xl font-bold'>
           Wayne Jones
         </Link>
-        <div className='hidden md:flex items-center gap-x-6 xl:gap-x-8'>
+        <div className='hidden md:flex items-center gap-x-6 xl:gap-x-8 text-base'>
           {navigation.map((item) => (
-            <a key={item.name} href={item.href} className='text-base'>
+            <Link
+              key={item.name}
+              href={item.href}
+              className='hover:underline hover:underline-offset-4'
+            >
               {item.name}
-            </a>
+            </Link>
           ))}
           <DarkModeTrigger />
         </div>
-        <div className='flex md:hidden p-3'>
+      </nav>
+      <Sheet>
+        <SheetTrigger asChild>
           <button
-            type='button'
-            className='-m-1.5 inline-flex items-center justify-center rounded-md p-1.5 '
+            className='md:hidden'
+            aria-expanded={mobileMenuOpen}
+            aria-controls='mobile-menu'
             onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
           >
-            <span className='sr-only'>Open main menu</span>
+            <span className='sr-only'>Toggle Mobile Navigation</span>
             <BiMenuAltRight className='h-8 w-8' aria-hidden='true' />
           </button>
-        </div>
-      </nav>
-      <Dialog as='div' className='md:hidden' open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-        <div className='fixed inset-0 z-10' />
-        <DialogPanel className='fixed inset-y-0 right-0 z-10 w-full overflow-y-auto px-4 sm:px-6 py-6 bg-white dark:bg-dark-gray'>
-          <div className='flex items-center justify-between'>
-            <Link href='/' className='uppercase text-2xl font-bold'>
-              Wayne Jones
-            </Link>
-            <div className='flex p-3'>
-              <button
-                data-autofocus
-                className='-m-1.5 inline-flex items-center justify-center rounded-md p-1.5'
-                onClick={() => setMobileMenuOpen(false)}
+        </SheetTrigger>
+        <SheetContent
+          side='right'
+          className='md:hidden'
+          id='mobile-menu'
+          aria-hidden={!mobileMenuOpen}
+        >
+          <SheetTitle className='sr-only'>Navigation Menu</SheetTitle>
+          <SheetDescription className='sr-only'>
+            Access the main navigation menu on mobile devices.
+          </SheetDescription>
+          <nav className='grid gap-4 p-6 text-lg font-medium'>
+            {navigation.map((item) => (
+              <Link
+                key={item.name}
+                href={item.href}
+                className='hover:underline hover:underline-offset-4'
               >
-                <span className='sr-only'>Close mobile menu</span>
-                <HiXMark className='h-8 w-8' aria-hidden='true' />
-              </button>
-            </div>
-          </div>
-          <div className=''>
-            <div className='space-y-2 flex flex-col'>
-              {navigation.map((item) => (
-                <a key={item.name} href={item.href} className='-mx-3 py-2 px-3 text-base'>
-                  {item.name}
-                </a>
-              ))}
-            </div>
-          </div>
-        </DialogPanel>
-      </Dialog>
+                {item.name}
+              </Link>
+            ))}
+            <DarkModeTrigger />
+          </nav>
+        </SheetContent>
+      </Sheet>
     </header>
   );
 };
